91702ced5f5dc743e2fa155fe46ccb959071274b
Waventus <102645937+LanVVVVV@users.noreply.github.com>
2026-06-30 00:30:54 +0800

    Feature: Added height adjustment support for extra character info blocks in the info panel window

---
+++ MoreGraphicAttachments/Features/InformationLookWindowHeight.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace MoreGraphicAttachments.Features;
+
+internal static class InformationLookWindowHeight
+{
+    //cellSize = (148, 54)
+    private const float CellHeight = 54f;
+
+    //spacing = (0, 1)
+    private const float Spacing = 1f;
+
+    //private const float RightOverGap = 36f;
+
+    //FemaleInformationLook: (524 838)
+    internal static Vector2 AdjustHeight(Vector2 sizeDelta, int leftCount, int rightCount)
+    {
+        //leftCount = leftCount - 1;
+
+        int count = Mathf.Max(leftCount, rightCount);
+
+        if(count == 0) return sizeDelta;
+
+        //if (leftCount < rightCount)
+        //{
+        //    sizeDelta.y -= RightOverGap;
+        //}
+
+        sizeDelta.y += (CellHeight + Spacing) * count;
+
+        return sizeDelta;
+    }
+}
