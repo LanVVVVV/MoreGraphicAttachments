e5e2a42fb23f8c73a2c13b871e839315c64ff078
Waventus <102645937+LanVVVVV@users.noreply.github.com>
2026-07-03 18:26:36 +0800

    OpenWindow

---
+++ MoreGraphicAttachments/Patches/PlayDataPatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using MBMScripts;
+
+namespace MoreGraphicAttachments.Patches;
+
+[HarmonyPatch(typeof(PlayData))]
+public static class SelectedUnitPatch
+{
+    [HarmonyPatch(nameof(PlayData.SelectedUnit), MethodType.Setter)]
+    [HarmonyPostfix]
+    public static void SelectedUnitPostfix(Unit value)
+    {
+        if (value is null) return;
+
+        if (value is not Amilia && value is not Flora && value is not Niel && value is not SenaLena && value is not Barbara)
+        {
+            return;
+        }
+        GameManager.Instance.OpenWindow(EGameWindow.FemaleInformation);
+    }
+}
